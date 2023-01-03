da7e4d76c6c898c7611f7dfa42de5a7f49bd731b
Chandanad321 <121717744+Chandanad321@users.noreply.github.com>
2023-01-03 16:12:22 +0530

    Add files via upload

--- program11.py
+++ program11.py
@@ -1,8 +1,10 @@
-sample_dict={
-    "name":"Chandrakala",
-    "age":27,
-     "salary":90000,
-     "city":"Krishna rajpuram"
-}
-sample_dict['location']=sample_dict.pop('city')
-print(sample_dict)
+class Number:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+n1=int(input())
+n2=int(input())
+ob=Number(n1,n2)
+print(n1,'+',n2,'=',ob.add(),sep='')
