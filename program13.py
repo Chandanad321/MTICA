74df808329b5788c839a71e87cdfe56dbe2696d0
Chandanad321 <121717744+Chandanad321@users.noreply.github.com>
2023-01-03 16:13:00 +0530

    Add files via upload

--- program13.py
+++ program13.py
@@ -1,7 +1,25 @@
-sample_dict={
-    'emp1':{'name':'Jhon','salary':5000},
-     'emp2':{'name':'Women','salary':7000},
-     'emp3':{'name':'Man','salary':600},
-}
-sample_dict['emp3']['salary']=7500
-print(sample_dict)
+class Number:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+    def sub(self):
+        return self.num1-self.num2
+    def mul (self):
+        return self.num1*self.num2
+    def div(self):
+        assert(self.num2!=0),"number is divisible by 0"
+        return self.num1/self.num2
+    
+n1=int(input())
+n2=int(input())
+ob=Number(n1,n2)
+try:
+    print(n1,'/',n2,'=',ob.div(),sep='')
+except ZeroDivisionError as obj:
+    print(obj)
+print(n1,'+',n2,'=',ob.add(),sep='')
+print(n1,'-',n2,'=',ob.sub(),sep='')
+print(n1,'*',n2,'=',ob.mul(),sep='')
+print(n1,'/',n2,'=',ob.div(),sep='')
